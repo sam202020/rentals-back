59cf885a59470f1d60ebb56c9f9458035aea23c5
Shalom <shalom@Shaloms-MBP.home>
2019-04-01 12:18:45 -0400

    update middleware

--- src/middleware/scraper.js
+++ src/middleware/scraper.js
@@ -1,5 +1,10 @@
 const cheerio = require('cheerio')
 const rp = require('request-promise');
+const fs = require('fs')
+
+let obj = {
+  table: []
+}
 
 const options = {
     uri: `https://www.apartments.com/lakewood-nj/`,
@@ -7,18 +12,43 @@ const options = {
       return cheerio.load(body);
     }
   };
-
-export default getDefaultApartments = options => {
   rp(options)
   .then(($) => {
-    // console.log($('.placardTitle').text())
-    // console.log($('.altRentDisplay'))
-    // console.log($('.phone'))
-    $(".placardTitle" ).each(function( index ) {
-        console.log( index + ": " + $( this ).text() );
-      });
-  })
-  .catch((err) => {
-    console.log(err);
+  $(".placardTitle" ).each(function( index ) {
+    console.log( index + ": " + $( this ).text() );
+    var place = $( this ).text();
+    var strippedPlace = place.substring(1);
+    obj.table.push({'type': 'apartment', 'place': strippedPlace})
   });
+  $(".altRentDisplay" ).each(function( index ) {
+    console.log( index + ": " + $( this ).text() );
+    obj.table[index]['price'] = $( this ).text();
+  });
+  $(".unitLabel" ).each(function( index ) {
+    console.log( index + ": " + $( this ).text() );
+    var beds = $( this ).text();
+    var strippedBeds = beds.replace(/[^0-9\-]/g,'');
+    obj.table[index]['bedrooms'] = strippedBeds;
+  });
+  $(".phone" ).each(function( index ) {
+    console.log( index + ": " + $( this ).text() );
+    var string = $( this ).text();
+    var stripped = string.replace(/[^0-9\-]/g,'');
+    obj.table[index]['phone'] = stripped;
+  });
+}).then(()=>{
+  var json = JSON.stringify(obj);
+  fs.writeFile('myjsonfile.json', json, 'utf8', callback);
+})
+.catch((err) => {
+  console.log(err);
+});
+
+const callback = () => {
+  console.log('hello')
 };
+
+
+  
+
+
--- src/models/Rental.js
+++ src/models/Rental.js
@@ -1,89 +1,63 @@
-const mongoose = require('mongoose');
-const nodemailer = require('nodemailer');
-
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'shalomlandsman@gmail.com',
-        pass: 'azarya2020'
-    }
-});
+const mongoose = require("mongoose");
 
 const RentalModel = mongoose.Schema({
-    place: {
-        type: String,
-        required: true,
-    },
-    type: {
-        type: String,
-        required: true,
-    },
-    bedrooms: {
-        type: String,
-        required: true,
-    },
-    baths: {
-        type: String,
-        required: true,
-    },
-    wePay: {
+  place: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  bedrooms: {
+    type: String,
+    required: true
+  },
+  baths: {
+    type: String,
+    required: true
+  },
+  wePay: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  price: {
+    type: Number
+  },
+  comments: {
+    type: String
+  },
+  pictures: {
+    type: Array
+  },
+  createOn: {
+    type: Date,
+    default: Date.now()
+  },
+  hud: {
+    type: Boolean
+  },
+  address: {
+    type: Object
+  },
+  user: {
+    type: String
+  },
+  inquries: [
+    {
+      user: {
         type: String
-    },
-    phone: {
-        type: String,
-    },
-    email: {
+      },
+      message: {
         type: String
-    },
-    price: {
-        type: Number
-    },
-    comments: {
-        type: String
-    },
-    pictures: {
-        type: Array
-    },
-    createOn: {
-        type: Date,
-        default: Date.now()
-    },
-    hud: {
-        type: Boolean
-    },
-    address: {
-        type: Object,
-    },
-    user: {
-        type: String
-    },
-    inquries: [{
-        user: {
-            type: String
-        },
-        message: {
-            type: String
-        }
-    }],
+      }
+    }
+  ]
 });
 
-RentalModel.post('save', function (doc) {
-    console.log('rental added');
-    let mailOptions = {
-        from: 'shalomlandsman@gmail.com',
-        to: 'shalomlandsman@gmail.com',
-        subject: 'Rental added',
-        text: String(doc._id)
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-
-
-});
-
-module.exports = mongoose.model('rentals', RentalModel);
+module.exports = mongoose.model("rentals", RentalModel);
